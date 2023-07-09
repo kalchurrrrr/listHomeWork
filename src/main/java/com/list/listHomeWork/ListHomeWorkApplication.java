635d192aa4b1d90354d3c13bd3e3a766635c4a08
kalchurrrrr <nikolaykalchu@gmail.com>
2023-07-09 13:38:13 +0300

    Initial commit

---
+++ src/main/java/com/list/listHomeWork/ListHomeWorkApplication.java
@@ -0,0 +1,13 @@
+package com.list.listHomeWork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListHomeWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListHomeWorkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/list/listHomeWork/ListHomeWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.list.listHomeWork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListHomeWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
